d29f06cb0058fc11921918bab5669a985c54cb49
Artsiom-Zhuk <artsiom.zhuk498@gmail.com>
2019-02-24 23:09:54 +0300

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
----
-__Instructions for using the nodejs script__
-
-- Place the necessary exel files in the `data` folder.
-- Run the script with the `node parseExel` command.
-- The finished json file is in the `public` folder.
---
+++ precache-manifest.b75b4d59a7433391f9b7e70d1ecfc03d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5280a2c0b2d5e5880bdc",
+    "url": "/rss-mentor-dashboard/static/js/runtime~main.5280a2c0.js"
+  },
+  {
+    "revision": "8b8ea938d0cc2d7cc653",
+    "url": "/rss-mentor-dashboard/static/js/main.8b8ea938.chunk.js"
+  },
+  {
+    "revision": "0f8919162bd460fc2f84",
+    "url": "/rss-mentor-dashboard/static/js/2.0f891916.chunk.js"
+  },
+  {
+    "revision": "8b8ea938d0cc2d7cc653",
+    "url": "/rss-mentor-dashboard/static/css/main.8d1d7e23.chunk.css"
+  },
+  {
+    "revision": "752bcdd4d35c784e715f0baa49fc8ecf",
+    "url": "/rss-mentor-dashboard/index.html"
+  }
+];
